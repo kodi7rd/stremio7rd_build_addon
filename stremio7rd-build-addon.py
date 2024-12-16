e512bd4eba9cddc14875148e9c5fc5364f6faf68
kodi7rd <KodiRealDebridIsrael@github.com>
2024-12-17 00:36:07 +0200

    Update stremio7rd-build-addon.py

--- stremio7rd-build-addon.py
+++ stremio7rd-build-addon.py
@@ -8,7 +8,7 @@ STREMIO7RD_URL = "https://i.imgur.com/CRpsxpE.jpeg"
 RED_THUMBS_UP_URL = "https://i.imgur.com/HwcDn4G.png"
 GREEN_THUMBS_UP_URL = "https://i.imgur.com/aCNdSXk.png"
 NEW_UPDATE_URL = "https://i.imgur.com/RZcmX2e.png"
-BUILD_QR_URL = "https://i.imgur.com/pxbE5CE.png"
+BUILD_QR_URL = "https://i.imgur.com/wbnhJUp.png"
 
 # Define a base manifest template
 def generate_manifest(version):
