c2b83db69ee081c6fff51d36755105e587cc2557
kodi7rd <KodiRealDebridIsrael@github.com>
2024-12-17 01:25:49 +0200

    Update stremio7rd-build-addon.py

--- stremio7rd-build-addon.py
+++ stremio7rd-build-addon.py
@@ -122,4 +122,4 @@ if __name__ == "__main__":
     threading.Thread(target=keep_server_alive, daemon=True).start()
     
     print("[DEBUG] Starting the Flask application on http://0.0.0.0:5000")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
