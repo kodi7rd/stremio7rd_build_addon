95e8537184e5e1cc89b48ae3a30a574131c43400
kodi7rd <KodiRealDebridIsrael@github.com>
2024-12-16 05:07:50 +0200

    Revert "Update stremio7rd-build-addon.py"
    This reverts commit 2f04e8e944e4c3d454850d3f42e19e31dec625f8.

--- stremio7rd-build-addon.py
+++ stremio7rd-build-addon.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, redirect
 app = Flask(__name__)
 
 # Constants
-LATEST_VERSION = "1.0.0" 
+LATEST_VERSION = "1.0.0"
 STREMIO7RD_URL = "https://i.imgur.com/CRpsxpE.jpeg"
 RED_THUMBS_UP_URL = "https://i.imgur.com/HwcDn4G.png"
 GREEN_THUMBS_UP_URL = "https://i.imgur.com/dJ1E3aj.png"
